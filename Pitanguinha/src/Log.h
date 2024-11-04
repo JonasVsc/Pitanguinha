6ab9715562d801cd8586750f65e726e7922ae30c
jonasvsc <jonasvasconcelosbarbosa@gmail.com>
2024-11-04 19:01:06 -0300

    Init Log

---
+++ Pitanguinha/src/Log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+#ifdef PITANGUINHA_DEBUG
+	#define PTG_DEBUG(...) std::cout << "[Pitanguinha] " << __VA_ARGS__ << '\n'
+#else
+	#define PTG_DEBUG(...) 
+#endif
+
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,9 @@
 #include "Teste.h"
+#include "Log.h"
 
 int main()
 {
 	Ptg::Print();
-	
+
 	return 0;
 }
